1493997ce533f36935f465794639a8c1b20ed9ae
Eric Vennaro <ericsMac@Erics-MacBook-Pro.local>
2014-05-12 13:58:37 -0400

    Created Ability to post articles...now for comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,48 @@
 class ArticlesController < ApplicationController
 	def new
+		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
 	end
 
 	def create
-		@article = Article.new(params[:article])
+		@article = Article.new(article_params)
 
-		@article.save
-		redirect_to @article
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
 	end
+
+	def update
+		@article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+
+	def index
+		@articles = Article.all
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :text);
+		end
 end
