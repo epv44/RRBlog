f6622fd237f1a32ca3b8443c49dc93a13357dc1e
Eric <epv9@case.edu>
2014-07-03 14:48:30 -0400

    updated mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,16 +76,17 @@ Rails.application.configure do
   # config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.action_mailer.delivery_method = :smtp
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.smtp_settings = {
-    address: “smtp.gmail.com”,
-    port: 587,
-    domain: ENV["DOMAIN_NAME"],
-    authentication: “plain”,
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
-  }
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["DOMAIN_NAME"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
   # Do not dump schema after migrations.
   config.action_mailer.default_url_options = { host: 'www.blooming-chamber-9311.herokuap.com' }
   config.active_record.dump_schema_after_migration = false
