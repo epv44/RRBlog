4c78654b0d3c54ac1396c2dc7d1afdeb240a0fd5
Eric <epv9@case.edu>
2014-07-03 14:38:33 -0400

    css changes and email for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,16 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
+  config.action_mailer.smtp_settings = {
+    address: “smtp.gmail.com”,
+    port: 587,
+    domain: ENV["DOMAIN_NAME"],
+    authentication: “plain”,
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
   # Do not dump schema after migrations.
+  config.action_mailer.default_url_options = { host: 'www.murmuring-depths-4307.herokuap.com' }
   config.active_record.dump_schema_after_migration = false
 end
